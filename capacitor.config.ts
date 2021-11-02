430ad9dec8824d5c2dfaed5d9138c070fad031ef
Pavel40 <pav40@protonmail.com>
2021-11-03 00:40:41 +0100

    Set correct name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'habitApp',
+  appName: 'Habit',
   webDir: 'build',
   bundledWebRuntime: false
 };
